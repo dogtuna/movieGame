ae9df97cdad4b81959ee859a430a7a38634d0de6
Jonny! <jonny@Jonnys-Mac-mini.local>
2023-07-17 15:14:17 -0500

    Another test for deployment.

--- src/server/index.js
+++ src/server/index.js
@@ -22,10 +22,10 @@ function log(message) {
 }
 
 // Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
   });
 
 app.get("/api/actors", async (req, res) => {
